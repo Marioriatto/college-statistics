f9ba71bdb34bb3116ade94e1e1fcadb9cd16be48
Mario Rodríguez <mariocrg3@gmail.com>
2025-09-18 09:47:31 -0600

    Delete utils.py

--- utils.py
+++
@@ -1,3 +0,0 @@
-def variance(sample : list):
-    mean = (sum(sample)/len(sample))
-    return { "realVariance" : [abs(i - mean) for i in sample], "estimatedVariance" : (sum([( i - mean ) ** 2 for i in sample]) / ( len(sample) - 1 ))}
