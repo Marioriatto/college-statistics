072404d5319a25ce178c33ae51f1bddf01b77744
Mario Rodríguez <mariocrg3@gmail.com>
2025-09-18 19:47:54 -0600

    pepe chancho

--- predicting.py
+++ predicting.py
@@ -1,12 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
-from sklearn.feature_selection import f_regression, SelectKBest
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.decomposition import PCA
 df = pd.read_csv('./archive/data.csv')
 df = df.dropna()
 X = df[['90s','Min','MP']]
